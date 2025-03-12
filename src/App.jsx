b98413b9d5f339ea9ad29308ed14006b62c011f6
Kyrylo Golikov <pipecc1985@gmail.com>
2025-03-12 21:30:56 +0100

    add task solution

--- src/App.jsx
+++ src/App.jsx
@@ -26,8 +26,8 @@ export const alex = {
 
 export const App = () => (
   <div className="App">
-    <Person person={misha} wh="" />
-    <Person person={olya} wh="" />
-    <Person person={alex} wh="" />
+    <Person person={misha} />
+    <Person person={olya} />
+    <Person person={alex} />
   </div>
 );
